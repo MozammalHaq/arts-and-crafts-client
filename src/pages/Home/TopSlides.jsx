ddce3f878b2d1043cc5db7cdf54ed9e9ff4ae995
MozammalHaq <mrmozammal@gmail.com>
2023-06-08 23:30:35 +0600

    top banner added

--- src/pages/Home/TopSlides.jsx
+++ src/pages/Home/TopSlides.jsx
@@ -1,20 +1,44 @@
-import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+import banner1 from '../../assets/images/banner/banner-1.jpg'
+import banner2 from '../../assets/images/banner/banner-2.jpg'
+import banner3 from '../../assets/images/banner/banner-3.jpg'
+import banner4 from '../../assets/images/banner/banner-4.jpg'
+import banner5 from '../../assets/images/banner/banner-5.jpg'
+import banner6 from '../../assets/images/banner/banner-6.jpg'
+import banner7 from '../../assets/images/banner/banner-7.jpg'
 
 const TopSlides = () => {
     return (
         <Carousel>
             <div>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Neolithic_Majiayao_Culture_Pottery_03.jpg/220px-Neolithic_Majiayao_Culture_Pottery_03.jpg" />
-                <p className="legend">Drawing</p>
+                <img src={banner1} />
+                <p className="legend">Decorative Art</p>
             </div>
             <div>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh5DMep-cAmLCC-UH4Ih4bNpalQKtttELJPQ&usqp=CAU" />
-                <p className="Drawing">Legend 2</p>
+                <img src={banner2} />
+                <p className="legend">Applied Art</p>
             </div>
             <div>
-                <img src="https://www.fashionlady.in/wp-content/uploads/2017/10/paper-craft-ideas-for-decoration-For-house.jpg" />
+                <img src={banner3} />
+                <p className="legend">Pottery Painting</p>
+            </div>
+            <div>
+                <img src={banner4} />
                 <p className="legend">Paper Craft</p>
+            </div>
+            <div>
+                <img src={banner5} />
+                <p className="legend">Painting</p>
+            </div>
+            <div>
+                <img src={banner6} />
+                <p className="legend">Paint Making</p>
+            </div>
+            <div>
+                <img src={banner7} />
+                <p className="legend">Jewelry</p>
             </div>
         </Carousel>
     );
