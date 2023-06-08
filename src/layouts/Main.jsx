3c5aaa8c25d855bcabd603d9eb23dd85c8415dd0
MozammalHaq <mrmozammal@gmail.com>
2023-06-09 02:11:48 +0600

    Added fotter section

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Shared/Navbar";
+import Footer from "../components/Shared/Footer";
 
 const Main = () => {
   return (
     <div className="bg-[#1D1E23] text-yellow-600">
       <Navbar />
       <Outlet />
+      <Footer/>
     </div>
   )
 }
